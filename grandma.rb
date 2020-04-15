8f887be8a7c560cdb7d4f54e2098d936d3502e84
Erin Mettille <ecmettille@gmail.com>
2020-04-15 17:25:18 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -16,8 +16,8 @@ def speak_to_grandma(greeting)
   if greeting.upcase == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
   elsif greeting.upcase == greeting
-    "HUH?! SPEAK UP, SONNY!"
-  else 
     "NO, NOT SINCE 1938!"
+  else 
+    "HUH?! SPEAK UP, SONNY!"
   end
 end
